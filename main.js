0a215677ec26c5a6ddd272e068476ab37562aec5
kristinadhakal <112728281+kristinadhakal@users.noreply.github.com>
2022-12-16 14:48:01 -0700

    hi

--- main.js
+++ main.js
@@ -1,47 +1,73 @@
 // My Contacts Basic
 
 // HTML Elements
-let goBtnEl = document.getElementById('go-btn');
-let menuEl = document.getElementById('menu');
-let outputEl = document.getElementById('output');
+let goBtnEl = document.getElementById("go-btn");
+let menuEl = document.getElementById("menu");
+let outputEl = document.getElementById("output");
+
+// Global Variables
+let output = [];
 
 // Go Btn - Menu Listener
-goBtnEl.addEventListener('click', goBtnHandler);
+goBtnEl.addEventListener("click", goBtnHandler);
 
 function goBtnHandler() {
   // Get Menu Selection
   let selection = menuEl.value;
 
-  if (selection === 'display-all') {
+  if (selection === "display-all") {
     displayContacts();
-  } else if (selection === 'add') {
+  } else if (selection === "add") {
     addContact();
-  } else if (selection === 'remove') {
+  } else if (selection === "remove") {
     removeContact();
-  } else if (selection === 'display-name') {
+  } else if (selection === "display-name") {
     displayByName();
-  } else if (selection === 'display-country') {
+  } else if (selection === "display-country") {
     displayByCountry();
   }
 }
 
 // MENU FUNCTIONS
 function displayContacts() {
-  console.log('Display Contacts');
+  console.log("Display Contacts");
+  outputEl.innerHTML = ""; // Clear the output
+
+  // Add each contact to the output
+  for (let i = 0; i < output.length; i++) {
+    let divEl = document.createElement("div");
+    divEl.innerHTML = output[i].name;
+    outputEl.appendChild(divEl);
+  }
 }
 
 function addContact() {
-  console.log('Add Contact');
+  let name = prompt("Enter name:");
+  let email = prompt("Enter email:");
+  let phoneNum = prompt("Enter phone number:");
+
+  output.push(returnObject(name, email, phoneNum));
 }
 
 function removeContact() {
-  console.log('Remove Contact');
+  console.log("Remove Contact");
 }
 
 function displayByName() {
-  console.log('Display by Name');
+  console.log("Display by Name");
 }
 
 function displayByCountry() {
-  console.log('Display by Country');
+  console.log("Display by Country");
+}
+
+// Helper
+// new name
+
+function returnObject(nameParameter, emailParameter, phoneNumParameter) {
+  return {
+    name: nameParameter,
+    email: emailParameter,
+    phoneNum: phoneNumParameter,
+  };
 }
